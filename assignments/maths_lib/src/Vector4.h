ef894d9cb99639840ee9228b26fd9716e8514e32
Borgosity <borgosity@gmail.com>
2016-05-13 09:41:20 +1000

    merge matrix and vector headers

--- assignments/maths_lib/src/Vector4.h
+++ assignments/maths_lib/src/Vector4.h
@@ -1,45 +1,45 @@
 #pragma once
-#include "Vector2.h"
-#include "Vector3.h"
-//#include "Matrix4.h"
-#include <math.h>
+//#include "Vector2.h"
+//#include "Vector3.h"
+////#include "Matrix4.h"
+//#include <math.h>
 
-class Vector4
-{
-public:
-	float m_w;
-	float m_x;
-	float m_y;
-	float m_z;
-
-	Vector4();
-	Vector4(float x, float y, float z, float w);
-	Vector4(Vector4 & v4);
-	Vector4(Vector3 & v3);
-	Vector4(Vector2 & v2);
-	~Vector4();
-
-	Vector4 scale(float scaler);
-	Vector4 cross(Vector4 & v4);
-	float dot(Vector4 & v4);
-	float magnitude();
-	void normalise();
-	operator float*();
-
-	// operator overloads
-	Vector4 & operator=(Vector4 & v4);
-	Vector4 & operator=(Vector3 & v3);
-	Vector4 & operator=(Vector2 & v2);
-	Vector4 & operator+=(const Vector4 & v4b);
-	friend Vector4 operator+ (Vector4 & v4a, Vector4 & v4b);
-	Vector4 & operator-=(const Vector4 & v4b);
-	friend Vector4 operator- (Vector4 & v4a, Vector4 & v4b);
-	Vector4 & operator*=(const Vector4 & v4b);
-	friend Vector4 operator* (Vector4 & v4a, Vector4 & v4b);
-	friend Vector4 operator* (float f, Vector4 & v4);
-	friend Vector4 operator* (Vector4 & v4, float f);
-	//friend Vector4 operator* (Vector4 & v4, Matrix3 & m4);
-	//friend Vector4 operator* (Matrix4 & m4, Vector3 & v4);
-};
+//class Vector4
+//{
+//public:
+//	float m_w;
+//	float m_x;
+//	float m_y;
+//	float m_z;
+//
+//	Vector4();
+//	Vector4(float x, float y, float z, float w);
+//	Vector4(Vector4 & v4);
+//	Vector4(Vector3 & v3);
+//	Vector4(Vector2 & v2);
+//	~Vector4();
+//
+//	Vector4 scale(float scaler);
+//	Vector4 cross(Vector4 & v4);
+//	float dot(Vector4 & v4);
+//	float magnitude();
+//	void normalise();
+//	operator float*();
+//
+//	// operator overloads
+//	Vector4 & operator=(Vector4 & v4);
+//	Vector4 & operator=(Vector3 & v3);
+//	Vector4 & operator=(Vector2 & v2);
+//	Vector4 & operator+=(const Vector4 & v4b);
+//	friend Vector4 operator+ (Vector4 & v4a, Vector4 & v4b);
+//	Vector4 & operator-=(const Vector4 & v4b);
+//	friend Vector4 operator- (Vector4 & v4a, Vector4 & v4b);
+//	Vector4 & operator*=(const Vector4 & v4b);
+//	friend Vector4 operator* (Vector4 & v4a, Vector4 & v4b);
+//	friend Vector4 operator* (float f, Vector4 & v4);
+//	friend Vector4 operator* (Vector4 & v4, float f);
+//	//friend Vector4 operator* (Vector4 & v4, Matrix3 & m4);
+//	//friend Vector4 operator* (Matrix4 & m4, Vector3 & v4);
+//};
 
 
