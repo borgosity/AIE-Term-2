3da9c9de9cf777aee61312d23cc836f7517a1cbf
Borgosity <borgosity@gmail.com>
2016-05-13 16:06:06 +1000

    replaced header with combined vertor matrix header

--- assignments/maths_lib/src/Matrix2.h
+++ assignments/maths_lib/src/Matrix2.h
@@ -1,30 +1,30 @@
 #pragma once
-#include "Vector2.h"
+//#include "Vector2.h"
 //class Vector2;
 
-class Matrix2
-{
-public:
-	// member variables
-	Vector2 * m_column1 = new Vector2;
-	Vector2 * m_column2 = new Vector2;
-
-	// constructors
-	Matrix2();
-	Matrix2(int a);
-	Matrix2(float c1x, float c1y, 
-		    float c2x, float c2y);
-	Matrix2(Vector2 & c1v, Vector2 & c2v);
-	Matrix2(Matrix2 & m2);
-	~Matrix2();
-
-	// memeber functions
-	void setRotateZ(float z);
-
-	// operator overloads
-	Matrix2 &operator= (Matrix2 & m2);
-	operator float*();
-	Matrix2 &operator*= (const Matrix2 & m2b);
-	friend Matrix2 operator* (Matrix2 & m2a, Matrix2 & m2b);
-};
+//class Matrix2
+//{
+//public:
+//	// member variables
+//	Vector2 * m_column1 = new Vector2;
+//	Vector2 * m_column2 = new Vector2;
+//
+//	// constructors
+//	Matrix2();
+//	Matrix2(int a);
+//	Matrix2(float c1x, float c1y, 
+//		    float c2x, float c2y);
+//	Matrix2(Vector2 & c1v, Vector2 & c2v);
+//	Matrix2(Matrix2 & m2);
+//	~Matrix2();
+//
+//	// memeber functions
+//	void setRotateZ(float z);
+//
+//	// operator overloads
+//	Matrix2 &operator= (Matrix2 & m2);
+//	operator float*();
+//	Matrix2 &operator*= (const Matrix2 & m2b);
+//	friend Matrix2 operator* (Matrix2 & m2a, Matrix2 & m2b);
+//};
 
--- assignments/maths_lib/src/Matrix3.h
+++ assignments/maths_lib/src/Matrix3.h
@@ -1,34 +1,34 @@
 #pragma once
-#include "Vector3.h"
-
-
-class Matrix3
-{
-public:
-	// member variables
-	Vector3 * m_column1 = new Vector3;
-	Vector3 * m_column2 = new Vector3;
-	Vector3 * m_column3 = new Vector3;
-
-	// constructors
-	Matrix3();
-	Matrix3(int a);
-	Matrix3(float c1x, float c1y, float c1z, 
-			float c2x, float c2y, float c2z,
-			float c3x, float c3y, float c3z);
-	Matrix3(Vector3 & c1v, Vector3 & c2v, Vector3 & c3v);
-	Matrix3(Matrix3 & m3);
-	~Matrix3();
-
-	// member functions
-	void setRotateX(float x);
-	void setRotateY(float y);
-	void setRotateZ(float z);
-
-	// operator overloads
-	Matrix3 &operator= (Matrix3 & m3);
-	operator float*();
-	Matrix3 &operator*= (const Matrix3 & m3b);
-	friend Matrix3 operator* (Matrix3 & m3a, Matrix3 & m3b);
-};
+//#include "Vector3.h"
+//
+//
+//class Matrix3
+//{
+//public:
+//	// member variables
+//	Vector3 * m_column1 = new Vector3;
+//	Vector3 * m_column2 = new Vector3;
+//	Vector3 * m_column3 = new Vector3;
+//
+//	// constructors
+//	Matrix3();
+//	Matrix3(int a);
+//	Matrix3(float c1x, float c1y, float c1z, 
+//			float c2x, float c2y, float c2z,
+//			float c3x, float c3y, float c3z);
+//	Matrix3(Vector3 & c1v, Vector3 & c2v, Vector3 & c3v);
+//	Matrix3(Matrix3 & m3);
+//	~Matrix3();
+//
+//	// member functions
+//	void setRotateX(float x);
+//	void setRotateY(float y);
+//	void setRotateZ(float z);
+//
+//	// operator overloads
+//	Matrix3 &operator= (Matrix3 & m3);
+//	operator float*();
+//	Matrix3 &operator*= (const Matrix3 & m3b);
+//	friend Matrix3 operator* (Matrix3 & m3a, Matrix3 & m3b);
+//};
 
